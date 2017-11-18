ec1043606d02dde95fe47c0cab645c2bedf88ae0
Scott DeVito <scottadevito@gmail.com>
2017-11-18 16:38:41 -0500

    Start creating UI for Login

--- src/screens/login_screen.js
+++ src/screens/login_screen.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const LoginScreen = () => {
-  return <div>login</div>;
+  return (
+    <div className="login-screen">
+      <MuiThemeProvider>
+        <h1>Bad Beer Tasting</h1>
+
+        <TextField floatingLabelText="Username" />
+        <TextField floatingLabelText="Password" />
+        <RaisedButton className="login-button" label="Login" primary={true} />
+      </MuiThemeProvider>
+    </div>
+  );
 };
 
 export default LoginScreen;
