c14361b04c32dad283e3f9a04bc625de9a45677b
Scott DeVito <scottadevito@gmail.com>
2017-11-19 12:41:08 -0500

    Create initial UI for MainScreen

--- src/screens/main_screen.js
+++ src/screens/main_screen.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Divider from 'material-ui/Divider';
+import Timer from 'material-ui/svg-icons/image/timer';
+import { black500 } from 'material-ui/styles/colors';
 import { Link } from 'react-router-dom';
 
 const MainScreen = () => {
   return (
-    <div>
-      <div>Main</div>
+    <div className="main-screen">
+      <Timer color={black500} className="timer-icon" />
+      <h2 className="timer-count">10:00</h2>
+
+      <Divider />
+
+      <h1>Game Buttons</h1>
+      <RaisedButton
+        className="game-button"
+        label="Get Started!"
+        primary={true}
+      />
+      <RaisedButton className="game-button" label="Vote Now" disabled={true} />
       <Link to="/results">
         <RaisedButton
-          className="results-button"
+          className="game-button"
           label="Results"
-          primary={true}
+          secondary={true}
         />
       </Link>
     </div>
