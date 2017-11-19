0f1d396c6758be600b925f0f2c996b3dde1cde3b
Scott DeVito <scottadevito@gmail.com>
2017-11-19 17:17:08 -0500

    Add linear progress bar to MainScreen
    Countdown bar

--- src/screens/main_screen.js
+++ src/screens/main_screen.js
@@ -5,18 +5,21 @@ import Timer from 'material-ui/svg-icons/image/timer';
 import { black500 } from 'material-ui/styles/colors';
 import { Link } from 'react-router-dom';
 
+import LinearProgressBarTimer from '../components/linear_progress_bar_timer';
+
 const MainScreen = () => {
   return (
     <div className="main-screen">
       <Timer color={black500} className="timer-icon" />
       <h2 className="timer-count">10:00</h2>
+      <LinearProgressBarTimer setMins={10} />
 
       <Divider />
 
       <h1>Game Buttons</h1>
       <RaisedButton
         className="game-button"
-        label="Get Started!"
+        label="Add Your Beer"
         primary={true}
       />
       <RaisedButton className="game-button" label="Vote Now" disabled={true} />
