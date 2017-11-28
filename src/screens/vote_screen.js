a03d4eda6ede36131e888b45079ea60f981701e2
Scott DeVito <scottadevito@gmail.com>
2017-11-27 23:57:39 -0500

    VoteScreen logic scaffolding
    // TODO: Add submit button (must have voted on 3 beers), style properly, use filter to remove beers once they're chosen

--- src/screens/vote_screen.js
+++ src/screens/vote_screen.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import Checkbox from 'material-ui/Checkbox';
+
+import { Tabs, Tab } from 'material-ui/Tabs';
+import SwipeableViews from 'react-swipeable-views';
+
+// TODO: Add submit button (must have voted on 3 beers), style properly, use filter to remove beers once they're chosen
 
 const data = [
   { beerName: 'Beer 1', username: 'some user 2', id: 0 },
@@ -22,7 +26,15 @@ const data = [
 class VoteScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = { open: false };
+
+    this.state = {
+      selectedBeerIds: {
+        0: '',
+        1: '',
+        2: '',
+      },
+      slideIndex: 0,
+    };
   }
 
   renderBeerList = data => {
@@ -30,17 +42,66 @@ class VoteScreen extends Component {
       return (
         <div key={item.id}>
           <Divider />
-          <ListItem leftCheckbox={<Checkbox />} primaryText={item.beerName} />
+          <ListItem
+            onClick={() => this.selectBeer(item.id)}
+            primaryText={item.beerName}
+          />
           <Divider />
         </div>
       );
     });
   };
 
+  // Only allow user to select 3 beers
+  // If the beer id already exists in the object, replace it
+  // Order is important
+  selectBeer = id => {
+    let { selectedBeerIds, slideIndex } = this.state;
+
+    let newSelectedBeerIds = {};
+
+    newSelectedBeerIds[slideIndex] = id;
+
+    this.setState({
+      selectedBeerIds: Object.assign(selectedBeerIds, newSelectedBeerIds),
+    });
+  };
+
+  handleSwipeChange = value => {
+    this.setState({
+      slideIndex: value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <h3>3 Worst Tasting Beers</h3>
+        <h3 className="vote-header">3 Worst Tasting Beers</h3>
+
+        <div>
+          <Tabs onChange={this.handleSwipeChange} value={this.state.slideIndex}>
+            <Tab label="#1" value={0} />
+            <Tab label="#2" value={1} />
+            <Tab label="#3" value={2} />
+          </Tabs>
+          <SwipeableViews
+            index={this.state.slideIndex}
+            onChangeIndex={this.handleSwipeChange}
+          >
+            <div>
+              <p>Worst tasting beer #1</p>
+              <h2>{this.state.selectedBeerIds[0]}</h2>
+            </div>
+            <div>
+              <p>Worst tasting beer #2</p>
+              <h2>{this.state.selectedBeerIds[1]}</h2>
+            </div>
+            <div>
+              <p>Worst tasting beer #3</p>
+              <h2>{this.state.selectedBeerIds[2]}</h2>
+            </div>
+          </SwipeableViews>
+        </div>
         <List>{this.renderBeerList(data)}</List>
       </div>
     );
