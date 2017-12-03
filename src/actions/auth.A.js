91135cd3ee004d7102df04865d02edd64cff8331
Scott DeVito <scottadevito@gmail.com>
2017-12-03 15:17:31 -0500

    Create createNewUser action and wire up to fbRegister action

--- src/actions/auth.A.js
+++ src/actions/auth.A.js
@@ -1,8 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  CREATE_NEW_USER_SUCCESS,
+  CREATE_NEW_USER_FAIL,
+} from './types';
 import firebase from 'firebase';
 import db from '../startup/db_init';
-
-// const createNewUser = () => {};
 
 // const setUserDbInfo = () => {};
 
@@ -13,19 +16,44 @@ const fbRegister = ({ email, password }) => {
         .auth()
         .createUserWithEmailAndPassword(email, password)
         .then(user => {
-          // dispatch(createNewUser(email));
-          // dispatch({ type: SET_USER_AUTH_INFO, payload: { user } });
-          // dispatch(setUserDbInfo(email));
+          dispatch(createNewUser(user));
+          // dispatch(setUserDbInfo(user));
           dispatch({ type: REGISTER_SUCCESS, payload: { user } });
           // fetch info for this user and set in store
           resolve(user);
         })
         .catch(error => {
           dispatch({ type: REGISTER_FAIL, payload: { error } });
+          console.error(error);
           reject(error);
         });
     });
   };
 };
 
-export { fbRegister };
+// Create a new user document in Firestore
+const createNewUser = ({ email, uid }) => {
+  return dispatch => {
+    db
+      .collection('users')
+      .add({
+        email: email.toLowerCase(),
+        beerId: '',
+        admin: false,
+        joinDate: new Date()
+          .toJSON()
+          .slice(0, 10)
+          .replace(/-/g, '/'),
+        uid: uid,
+      })
+      .then(function(docRef) {
+        dispatch({ type: CREATE_NEW_USER_SUCCESS });
+      })
+      .catch(function(error) {
+        dispatch({ type: CREATE_NEW_USER_FAIL });
+        console.error('Error creating new user: ', error);
+      });
+  };
+};
+
+export { fbRegister, createNewUser };
