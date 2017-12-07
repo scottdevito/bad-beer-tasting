6919165cff5533296fbf3c7132586b8bb71a7d93
Scott DeVito <scottadevito@gmail.com>
2017-12-07 11:10:59 -0500

    Comment out React Router imports to suppress warnings until they're needed

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class Login extends Component {
