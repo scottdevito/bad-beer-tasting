a8e45c60fbbe3a30639810e22fd9da7badc1055d
Scott DeVito <scottadevito@gmail.com>
2017-12-07 11:11:32 -0500

    Wire up fbLogout action to AuthReducer

--- src/reducers/auth.R.js
+++ src/reducers/auth.R.js
@@ -5,6 +5,9 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   SET_USER_AUTH_INFO,
+  CLEAR_USER_INFO,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
 } from '../actions/types';
 
 export default function(state = {}, action) {
@@ -13,14 +16,20 @@ export default function(state = {}, action) {
       return Object.assign({}, state, { auth: action.payload, loggedIn: true });
     case REGISTER_FAIL:
       return Object.assign({}, state, { loggedIn: false });
-    case FETCH_USER_DB_INFO:
-      return Object.assign({}, state, { userDbInfo: action.payload });
     case LOGIN_SUCCESS:
       return Object.assign({}, state, { loggedIn: true });
     case LOGIN_FAIL:
       return Object.assign({}, state, { loggedIn: false });
+    case LOGOUT_SUCCESS:
+      return Object.assign({}, state, { loggedIn: false });
+    case LOGOUT_FAIL:
+      return Object.assign({}, state, { loggedIn: true });
     case SET_USER_AUTH_INFO:
-      return Object.assign({}, state, { auth: action.payload, loggedIn: true });
+      return Object.assign({}, state, { auth: action.payload });
+    case CLEAR_USER_INFO:
+      return Object.assign({}, state, { auth: '' });
+    case FETCH_USER_DB_INFO:
+      return Object.assign({}, state, { userDbInfo: action.payload });
     default:
       return state;
   }
