ec8b264b240b5971c8be4c9a33f8eae8f1c48bff
Scott DeVito <scottadevito@gmail.com>
2017-12-07 23:58:14 -0500

    Fix duplicating issue with BeerInfoReducer

--- src/reducers/beer_info.R.js
+++ src/reducers/beer_info.R.js
@@ -3,7 +3,7 @@ import { FETCH_BEER_INFO, CLEAR_BEERS_INFO } from '../actions/types';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_BEER_INFO:
-      return [...state, ...action.payload];
+      return Object.assign([], state, [...action.payload]);
     case CLEAR_BEERS_INFO:
       return Object.assign(...state, []);
     default:
