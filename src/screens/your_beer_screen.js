fdc6c86c816be9312c9c57d616fbe4a5bea73456
Scott DeVito <scottadevito@gmail.com>
2017-12-13 23:01:48 -0500

    Add confirmation button step to beer submit process

--- src/screens/your_beer_screen.js
+++ src/screens/your_beer_screen.js
@@ -8,6 +8,43 @@ class YourBeerScreen extends Component {
   state = {
     name: '',
     description: '',
+    submit: false,
+    confirmedSubmit: false,
+  };
+
+  renderSubmitButton = () => {
+    let { confirmedSubmit } = this.state;
+
+    return this.state.submit ? (
+      <RaisedButton
+        className="your-beer-submit"
+        label={confirmedSubmit ? 'Submitting Beer...' : 'Confirm Submit'}
+        backgroundColor="#80e27e"
+        onClick={() => this.onAddBeerSubmit()}
+        disabled={confirmedSubmit}
+      />
+    ) : (
+      <RaisedButton
+        className="your-beer-submit"
+        label="Submit Beer"
+        primary={true}
+        onClick={() => this.confirmSubmit()}
+      />
+    );
+  };
+
+  confirmSubmit = () => {
+    this.setState((prevState, props) => {
+      return { submit: true };
+    });
+  };
+
+  onAddBeerSubmit = () => {
+    this.props.addNewBeer(this.state, this.props.userDbInfo);
+
+    this.setState((prevState, props) => {
+      return { confirmedSubmit: !prevState.confirmedSubmit };
+    });
   };
 
   onAddBeerInputChange = (event, fieldId) => {
@@ -16,10 +53,6 @@ class YourBeerScreen extends Component {
     this.setState((prevState, props) => {
       return { [fieldId]: value };
     });
-  };
-
-  onAddBeerSubmit = () => {
-    this.props.addNewBeer(this.state, this.props.userDbInfo);
   };
 
   displayBeerInfo = () => {
@@ -58,13 +91,7 @@ class YourBeerScreen extends Component {
               }}
               value={this.state.description}
             />
-
-            <RaisedButton
-              className="your-beer-submit"
-              label="Submit"
-              primary={true}
-              onClick={() => this.onAddBeerSubmit()}
-            />
+            {this.renderSubmitButton()}
           </div>
         )}
       </div>
